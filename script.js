7b9ec4b28e139bc42f0febd9620ec9dc660ebf81
tecnologia <77143340+tecnologia733@users.noreply.github.com>
2025-05-29 21:02:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,31 @@ fetch("https://liturgia.up.railway.app/v2/")
   .then(response => response.json())
   .then(data => {
     let liturgiaHTML = `<h2>${data.liturgia} - ${data.data}</h2>
-                        <p><strong>Cor Litúrgica:</strong> ${data.cor}</p>
-                        <h3>Coleta</h3>
-                        <p>${data.oracoes.coleta}</p>`;
+                        <p><strong>Cor Litúrgica:</strong> ${data.cor}</p>`;
 
-    // Exibir todas as leituras
+    // Exibir todas as orações (coleta, oferendas, comunhão)
+    liturgiaHTML += `<h3>Coleta</h3><p>${data.oracoes.coleta}</p>`;
+    liturgiaHTML += `<h3>Oferendas</h3><p>${data.oracoes.oferendas}</p>`;
+    liturgiaHTML += `<h3>Comunhão</h3><p>${data.oracoes.comunhao}</p>`;
+
+    // Exibir todas as leituras (primeira, segunda, salmo, evangelho)
     if (data.leituras) {
       Object.keys(data.leituras).forEach(tipoLeitura => {
-        data.leituras[tipoLeitura].forEach(leitura => {
+        const leitura = data.leituras[tipoLeitura];
+        if (typeof leitura === "object") {
           liturgiaHTML += `<h3>${tipoLeitura}</h3>
                            <p><strong>${leitura.referencia}</strong></p>
                            <p>${leitura.titulo}</p>
                            <p>${leitura.texto.replace(/\n/g, "<br>")}</p>`;
-        });
+        }
       });
     }
 
-    // Exibir todas as orações extras
-    if (data.oracoes.extras) {
-      liturgiaHTML += `<h3>Orações Extras</h3>`;
-      data.oracoes.extras.forEach(orcao => {
-        liturgiaHTML += `<h4>${orcao.titulo}</h4>
-                         <p>${orcao.texto.replace(/\n/g, "<br>")}</p>`;
+    // Exibir todas as antífonas
+    if (data.antifonas) {
+      liturgiaHTML += `<h3>Antífonas</h3>`;
+      Object.keys(data.antifonas).forEach(tipo => {
+        liturgiaHTML += `<h4>${tipo}</h4><p>${data.antifonas[tipo]}</p>`;
       });
     }
 
